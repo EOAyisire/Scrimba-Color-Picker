f507ba73b9783cfccca5a5a57666bdbf7574fd26
EOAyisire <EOAyisire@users.noreply.github.com>
2022-11-07 22:41:12 +0000

    js for dropdown functionality written

---
+++ index.js
@@ -0,0 +1,33 @@
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelector(".menu li");
+  const selected = dropdown.querySelector(".selected");
+
+  // click event to select el
+  select.addEventListener("click", () => {
+    // add clicked select style to selected element
+    select.classList.toggle("select-clicked");
+    //  add rotat to caret
+    caret.classList.toggle("caret-rotate");
+  });
+
+  options.forEach((option) => {
+    // add click event to option el
+    option.addEventListener("click", () => {
+      // change selected inner text to clicked option
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+
+      // remove active class from all options el
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+    });
+  });
+});
